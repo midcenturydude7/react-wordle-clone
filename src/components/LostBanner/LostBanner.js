da437045533758e3dcd40c175c6774025bf9d41f
midCenturyDude7 <mgriffes@gmail.com>
2023-02-06 22:21:28 -0800

    Completed Exercise #5

---
+++ src/components/LostBanner/LostBanner.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Banner from "../Banner/Banner";
+
+function LostBanner({ answer }) {
+  return (
+    <>
+      <Banner status="sad">
+        <p>
+          Sorry, the correct answer is <strong>{answer}</strong>
+        </p>
+      </Banner>
+    </>
+  );
+}
+
+export default LostBanner;
